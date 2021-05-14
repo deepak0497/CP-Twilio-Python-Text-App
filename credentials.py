6f1fd6b91ddbcd1a16e3f01cede19d536c4a8f49
deepak0497 <78999970+deepak0497@users.noreply.github.com>
2021-05-14 17:56:17 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "ACde13dcdfb43c6be4dd4cef2ad3c57628"
+auth_token = "1e90d56fe9d606d36864d0eb5492fc28"
+my_cell = "+91 96948 86686"
+my_twilio = "+15095658864"
